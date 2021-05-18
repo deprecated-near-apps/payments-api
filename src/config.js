a003d8258c863659b2dd4af0d8a49f34a1346d5f
Josh <thisisjoshford@gmail.com>
2021-05-17 18:37:34 -0700

    update credential path / contract name

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = "dev-1612494521961-7654170";
+const contractName = 'dev-1621301728289-99398561950429';
 
 module.exports = function getConfig(isServer = false) {
   let config = {
@@ -6,7 +6,7 @@ module.exports = function getConfig(isServer = false) {
     nodeUrl: "https://rpc.testnet.near.org",
     walletUrl: "https://wallet.testnet.near.org",
     helperUrl: "https://helper.testnet.near.org",
-    CONTRACT_NAME,
+    contractName,
   };
 
   if (process.env.REACT_APP_ENV !== undefined) {
@@ -28,7 +28,7 @@ module.exports = function getConfig(isServer = false) {
       nodeUrl: "https://rpc.mainnet.near.org",
       walletUrl: "https://wallet.near.org",
       helperUrl: "https://helper.mainnet.near.org",
-      CONTRACT_NAME: "near",
+      contractName: "near",
     };
   }
 
